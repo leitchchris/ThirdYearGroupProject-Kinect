eab53a4df1e5044e1cc9089c34be3bbe09d51b71
TinouFR <hocquard.etienne@gmail.com>
2013-03-14 01:54:17 +0000

    4th Step : Face reco first try

---
+++ FaceReco_Webcam/CMakeFiles/FaceRecoWebCam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FaceRecoWebCam.dir/FaceRecoWebCam.cpp.o"
+  "FaceRecoWebCam.pdb"
+  "FaceRecoWebCam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FaceRecoWebCam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
