22b98df3af191e2b85e5e0a44d9074b469c591a7
TinouFR <hocquard.etienne@gmail.com>
2013-03-20 20:51:04 +0000

    Try to get IR image actually not working

--- Infrared_Kinect/InfraKinect.cpp
+++ Infrared_Kinect/InfraKinect.cpp
@@ -12,8 +12,12 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "libfreenect.hpp"
 #include <opencv2/opencv.hpp>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include "libfreenect.hpp"
+#include "libfreenect_cv.h"
+#include "libfreenect_sync.h"
 
 using namespace std;
 using namespace cv;
@@ -33,7 +37,7 @@ RNG rng(12345);
 class MyFreenectDevice : public Freenect::FreenectDevice {
   public:
 	MyFreenectDevice(freenect_context *_ctx, int _index)
-		: Freenect::FreenectDevice(_ctx, _index), m_buffer_depth(FREENECT_DEPTH_11BIT),m_buffer_rgb(FREENECT_VIDEO_IR_8BIT), m_gamma(2048), m_new_rgb_frame(false), m_new_depth_frame(false),
+		: Freenect::FreenectDevice(_ctx, _index), m_buffer_depth(FREENECT_DEPTH_11BIT),m_buffer_rgb(FREENECT_VIDEO_RGB), m_gamma(2048), m_new_rgb_frame(false), m_new_depth_frame(false),
 		  depthMat(Size(640,480),CV_16UC1), rgbMat(Size(640,480),CV_8UC3,Scalar(0)), ownMat(Size(640,480),CV_8UC3,Scalar(0))
 	{
 		for( unsigned int i = 0 ; i < 2048 ; i++) {
@@ -44,7 +48,7 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 	}
 	// Do not call directly even in child
 	void VideoCallback(void* _rgb, uint32_t timestamp) {
-		//std::cout << "RGB callback" << std::endl;
+		std::cout << "RGB callback" << std::endl;
 		m_rgb_mutex.lock();
 		uint8_t* rgb = static_cast<uint8_t*>(_rgb);
 		rgbMat.data = rgb;
@@ -64,7 +68,7 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 	bool getVideo(Mat& output) {
 		m_rgb_mutex.lock();
 		if(m_new_rgb_frame) {
-			//cv::cvtColor(rgbMat, output, CV_RGB2BGR);
+			cv::cvtColor(rgbMat, output, CV_RGB2BGR);
 			m_new_rgb_frame = false;
 			m_rgb_mutex.unlock();
 			return true;
@@ -100,22 +104,120 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 	bool m_new_depth_frame;
 };
 
+
+IplImage *GlViewColor(IplImage *depth)
+{
+	static IplImage *image = 0;
+	if (!image) image = cvCreateImage(cvSize(640,480), 8, 3);
+	unsigned char *depth_mid = (unsigned char*)(image->imageData);
+	int i;
+	for (i = 0; i < 640*480; i++) {
+		int lb = ((short *)depth->imageData)[i] % 256;
+		int ub = ((short *)depth->imageData)[i] / 256;
+		switch (ub) {
+			case 0:
+			depth_mid[3*i+2] = 255;
+			depth_mid[3*i+1] = 255-lb;
+			depth_mid[3*i+0] = 255-lb;
+			break;
+			case 1:
+			depth_mid[3*i+2] = 255;
+			depth_mid[3*i+1] = lb;
+			depth_mid[3*i+0] = 0;
+			break;
+			case 2:
+			depth_mid[3*i+2] = 255-lb;
+			depth_mid[3*i+1] = 255;
+			depth_mid[3*i+0] = 0;
+			break;
+			case 3:
+			depth_mid[3*i+2] = 0;
+			depth_mid[3*i+1] = 255;
+			depth_mid[3*i+0] = lb;
+			break;
+			case 4:
+			depth_mid[3*i+2] = 0;
+			depth_mid[3*i+1] = 255-lb;
+			depth_mid[3*i+0] = 255;
+			break;
+			case 5:
+			depth_mid[3*i+2] = 0;
+			depth_mid[3*i+1] = 0;
+			depth_mid[3*i+0] = 255-lb;
+			break;
+			default:
+			depth_mid[3*i+2] = 0;
+			depth_mid[3*i+1] = 0;
+			depth_mid[3*i+0] = 0;
+			break;
+		}
+	}
+	return image;
+}
+
 //================Function Main==================
 int main( int argc, const char** argv )
 {
-	Mat rgb_frame;
-	Mat depthMat(Size(640,480),CV_16SC1);
-	Mat depthf;
+
+	while (true) {
+			char *irBuffer  = (char*) malloc( 640*480*sizeof(char));
+	char *irBufferTemp = 0;
+    char *rgbBuffer = 0;
+    uint32_t ts;
+    printf("Init\n");
+		/*	//-- 2. Read the video stream
+	
+		//Freenect::setVideoFormat(FREENECT_VIDEO_IR_8BIT);
+		IplImage *image = freenect_sync_get_rgb_cv(0);
+		if (!image) {
+			printf("Error: Kinect not connected?\n");
+			return -1;
+		}
+		cvCvtColor(image, image, CV_RGB2BGR);
+		IplImage *depth = freenect_sync_get_depth_cv(0);
+		if (!depth) {
+			printf("Error: Kinect not connected?\n");
+			return -1;
+		}*/
+		IplImage *image = 0; 
+		Mat Test();
+		if (!image) image = cvCreateImageHeader(cvSize(640,488), 8 , 1); 
+		unsigned int timestamp;
+		printf("Init 2\n");
+	    if( freenect_sync_get_video((void**)&irBufferTemp,&timestamp,0,FREENECT_VIDEO_IR_8BIT)) 
+	    	return NULL;
+	    
+	    else
+	    {	printf("Get img\n");
+	    	cvSetData(image, irBufferTemp, 640*3 ) ; 
+	    	printf("Display\n");
+			if(image != NULL)
+				cvShowImage("RGB", image);
+			//imwrite("Test.jpg", image);
+		}
+		//cvShowImage("Depth", GlViewColor(depth));
+	}
+	return 0;
+	
+	//Mat rgb_frame;
+	//Mat depthMat(Size(640,480),CV_16SC1);
+	//Mat depthf;
 
 	//-- 1. Load the cascades
-	if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-	if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+	// if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+	// if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
 	//-- 2. Read the video stream
-	Freenect::Freenect freenect;
+	/*Freenect::Freenect freenect;
     MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
     device.setVideoFormat(FREENECT_VIDEO_IR_8BIT );
-    device.startVideo();
+    device.startVideo();*/
+    
+    // while(true) { 
+    // 	freenect_sync_get_video((void**)&irBufferTemp, &ts, 0, FREENECT_VIDEO_IR_8BIT);
+    // 	memcpy( irBuffer, irBufferTemp, 640*480*sizeof(char));
+    // 	imshow("IR",irBuffer); 
+    // }
 	/*device.startDepth();
 	while( true )
 	{
@@ -138,7 +240,7 @@ int main( int argc, const char** argv )
     	int c = waitKey(10);
     	if( (char)c == 'c' ) { break; }
     }*/
-   return 0;
+   //return 0;
 }
 
 //================function detectAndDisplay================
