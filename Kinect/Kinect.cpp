b3c4553defe8708c2c64001b0a06bda06e003031
TinouFR <hocquard.etienne@gmail.com>
2013-03-26 19:06:25 +0000

    Add : First protoype, headers

---
+++ Kinect/Kinect.cpp
@@ -0,0 +1,10 @@
+/*
+* Author          : Etienne Hocquard
+* Last Modified   : 26th March 2013        Created  :  26th March 2013   
+* File            : Kinect.cpp
+* Target          : Kinect home automation project 
+* Version         : 1.0.0
+* Description     : Face identification for the project "home automation" - 3rd year Edinburh Napier University
+* Requires        : Kinect, freenect, openCV, haarcascade_eye_tree_eyeglasses.xml + haarcascade_frontalface_alt.xml
+* G++             : To define
+*/
